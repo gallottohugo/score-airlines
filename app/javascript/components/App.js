74e90fed2fcd50caec4293eb1fe7efc07eb77278
gallottohugo <gallottohugo@gmail.com>
2021-05-27 14:07:47 -0300

    js component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <center>
+            <div>
+                Hello World!
+            </div>
+        </center>
+        
+    )
+}
+
+export default App
