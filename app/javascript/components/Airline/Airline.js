49f57242ba516b2d2bef19559a19279ff6f4429f
gallottohugo <gallottohugo@gmail.com>
2021-05-29 09:41:13 -0300

    Add styles to components

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 
 const Airline = () => {
+    
+    
+    
     return <center><div>This is the Airline#SHOW view for our app.</div></center>
 }
 
