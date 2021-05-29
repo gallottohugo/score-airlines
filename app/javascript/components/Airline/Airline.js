a55cb29a0dfd316e672ea04155df7c62b30fe074
gallottohugo <gallottohugo@gmail.com>
2021-05-29 11:40:38 -0300

    -

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,10 +1,76 @@
 import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './Header'
+import styled from 'styled-components'
 
-const Airline = () => {
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Column = styled.div`
+  background: #fff; 
+  max-width: 50%;
+  width: 50%;
+  float: left; 
+  height: 100vh;
+  overflow-x: scroll;
+  overflow-y: scroll; 
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  &:last-child {
+    background: black;
+    border-top: 1px solid rgba(255,255,255,0.5);
+  }
+`
+
+const Main = styled.div`
+  padding-left: 60px;
+`
+
+
+const Airline = (props) => {
+    const [airline, setAirline] = useState({})
+    const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
     
+    useEffect(() => {
+        
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+
+        axios.get(url)
+        .then(resp => {
+            setAirline(resp.data)
+            setLoaded(true)
+        })
+        .catch(resp => console.log(resp))
+        
+
+    }, [])
     
-    
-    return <center><div>This is the Airline#SHOW view for our app.</div></center>
+    return (
+        <Wrapper>
+            <Column>
+                <Main>
+                    {
+                        loaded && 
+                        <Header 
+                            attributes={airline.data.attributes} 
+                            reviews={airline.included}
+                        />
+                    }
+                
+                    <div className="reviews"></div>
+                </Main>
+            </Column>
+            <Column>
+                <div className="review-form">[Review Form Goes Here.]</div>
+            </Column>
+        </Wrapper>
+    )
 }
 
 export default Airline
